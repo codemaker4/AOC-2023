03a03941b330f7ea45547d775c6b930c03454e2d
CodeMaker4 <codemaker4@gmail.com>
2023-12-03 12:01:17 +0100

    day 3 a working

---
+++ 3/a.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <string.h>
+#define BUFSIZE 1024
+
+// Returns the digit as an int, or -1 if it is not a digit.
+int digit(char c) {
+    int digit = (int) c - '0';
+    if (digit >= 0 && digit <= 9) {
+        return digit;
+    } else {
+        return -1;
+    }
+}
+
+// Reads an integer from the start of the string, and sets the pointer to the first non-digit char.
+// Returns the integer, or -1 if the string did not start with a valid int.
+int read_int(char **str) {
+    int num = -1;
+
+    while (1) {
+        int digit_read = digit(**str);
+        if (digit_read == -1) {
+            if (num == -1) {
+                (*str)++;
+            }
+            return num;
+        }
+        if (num == -1) {
+            num = digit_read;
+        } else {
+            num *= 10;
+            num += digit_read;
+        }
+        (*str)++;
+    }
+}
+
+int main(void) {
+    char line[BUFSIZE];
+    char input[BUFSIZE][BUFSIZE];
+    int row = 0;
+    int col = 0;
+
+    printf("\nINPUT:\n\n");
+
+    while (fgets(line, BUFSIZE, stdin)) {
+        if (line[0] == '\n' || line[0] == '\0') {
+            continue;
+        }
+        strcpy(input[row], line);
+        input[row][strlen(line) - 1] = '\0';
+        col = strlen(input[row]);
+        printf("%s\n", input[row]);
+        row++;
+    }
+
+    const int rows = row;
+    const int cols = col;
+    printf("ROWS: %d, COLS: %d\n", rows, cols);
+    int sum = 0;
+
+    printf("\nPROCESSED:\n\n");
+
+    for (row = 0; row < rows; row++) {
+        for (col = 0; col < cols; col++) {
+            char* ptr = &input[row][col];
+            int num = read_int(&ptr);
+            if (num != -1) {
+                int len = ptr - &input[row][col];
+                int should_be_counted = 0;
+
+                for (int srow = row - 1; srow <= row + 1; srow++) {
+                    for (int scol = col - 1; scol <= col + len; scol++) {
+                        if (srow < 0 || srow >= rows || scol < 0 || scol >= cols) {
+                            continue;
+                        }
+                        char c = input[srow][scol];
+                        if (c != '.' && c != 'x' && digit(c) == -1) {
+                            should_be_counted++;
+                        }
+                    }
+                }
+
+                if (should_be_counted) {
+                    sum += num;
+                    for (int wcol = col; wcol < col + len; wcol++) {
+                        input[row][wcol] = 'x';
+                    }
+                }
+            }
+        }
+        printf("%s\n", input[row]);
+    }
+
+    printf("SUM: %d\n", sum);
+
+
+    return 0;
+}
